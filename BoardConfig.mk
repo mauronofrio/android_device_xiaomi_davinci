mauronofrio/android_device_xiaomi_raphael

Add a script to auto-select the device model

BoardConfig.mk
@@ -93,6 +93,11 @@ TARGET_USERIMAGES_USE_F2FS := true
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_COPY_OUT_VENDOR := vendor
 
+#Init
+TARGET_INIT_VENDOR_LIB := libinit_raphael
+TARGET_RECOVERY_DEVICE_MODULES := libinit_raphael
+TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
+
 # Recovery
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"

init/Android.mk
@@ -1,11 +1,11 @@
 #
-# Copyright (C) 2019 The TwrpBuilder Open-Source Project
+# Copyright (C) 2017-2018 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,6 +16,13 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter raphael,$(TARGET_DEVICE)),)
-include $(call all-makefiles-under,$(LOCAL_PATH))
-endif
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES := \
+    system/core/base/include \
+    system/core/init
+LOCAL_SRC_FILES := init_raphael.cpp
+LOCAL_MODULE := libinit_raphael
+
+include $(BUILD_STATIC_LIBRARY)

mauronofrio/android_device_xiaomi_davinci

BoardConfig.mk
@@ -93,6 +93,11 @@ TARGET_USERIMAGES_USE_F2FS := true
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_COPY_OUT_VENDOR := vendor
 
+#Init
+TARGET_INIT_VENDOR_LIB := libinit_davinci
+TARGET_RECOVERY_DEVICE_MODULES := libinit_davinci
+TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
+
 # Recovery
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"

init/Android.mk
@@ -1,11 +1,11 @@
 #
-# Copyright (C) 2019 The TwrpBuilder Open-Source Project
+# Copyright (C) 2017-2018 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,6 +16,13 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter davinci,$(TARGET_DEVICE)),)
-include $(call all-makefiles-under,$(LOCAL_PATH))
-endif
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES := \
+    system/core/base/include \
+    system/core/init
+LOCAL_SRC_FILES := init_davinci.cpp
+LOCAL_MODULE := libinit_davinci
+
+include $(BUILD_STATIC_LIBRARY)